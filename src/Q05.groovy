2c59f5fc95af972b55b084fc0a7a0bc6608f109c
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-07-22 21:56:20 +0100

    Q05 - added code to take account of negative numbers

--- src/Q05.groovy
+++ src/Q05.groovy
@@ -1,3 +1,5 @@
+import static java.lang.Math.abs
+
 /**
  * Created by crk on 22/07/2016.
  */
@@ -6,9 +8,11 @@ print "Multiplication\nEnter 1st number: "
 int number1 = input.nextInt()
 print "Enter 2nd number: "
 int number2 = input.nextInt()
+int signOfNumber2 = (number2 < 0) ? -1 : 1
+number2 = abs(number2)
 int result = 0
 while (number2 > 0) {
     result = result + number1
     number2--
 }
-println "result = " + result
+println "result = " + result*signOfNumber2
