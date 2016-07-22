e956962457c5200200d92ddc1c6bc2dc7bcb1e2e
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-07-22 23:25:34 +0100

    Q06 - tweaks to deal with negative numbers

--- src/Q06.groovy
+++ src/Q06.groovy
@@ -1,3 +1,5 @@
+import static java.lang.Math.abs
+
 /**
  * Created by crk on 22/07/2016.
  */
@@ -6,10 +8,14 @@ print "Division\nEnter 1st number: "
 int number1 = input.nextInt()
 print "Enter 2nd number: "
 int number2 = input.nextInt()
+boolean negative1 = number1 < 0
+boolean negativeQuotient = negative1 != number2 < 0
 int quotient = 0
-int remainder = number1
+int remainder = abs(number1)
+number2 = abs(number2)
 while (remainder >= number2) {
     quotient++
     remainder = remainder - number2
 }
-println number1 + "/" + number2 + " = " + quotient + ", remainder " + remainder
+print number1 + "/" + number2 + " = " + (negativeQuotient ? 0 - quotient : quotient)
+println ", remainder " + (negative1 ? 0 - remainder : remainder)
