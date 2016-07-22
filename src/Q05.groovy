36e0fde8d177113d58735b3171fefe43062d6fb1
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-07-22 22:08:56 +0100

    Q05 - tidied up code for negative numbers

--- src/Q05.groovy
+++ src/Q05.groovy
@@ -8,11 +8,11 @@ print "Multiplication\nEnter 1st number: "
 int number1 = input.nextInt()
 print "Enter 2nd number: "
 int number2 = input.nextInt()
-int signOfNumber2 = (number2 < 0) ? -1 : 1
+boolean negativeNumber2 = (number2 < 0)
 number2 = abs(number2)
 int result = 0
 while (number2 > 0) {
     result = result + number1
     number2--
 }
-println "result = " + result*signOfNumber2
+println "result = " + (negativeNumber2 ? 0 - result : result)
