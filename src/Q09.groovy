caae790d651a2000bfa642a54fcea0e8620d27ef
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-07-23 23:02:38 +0100

    Q08 - adding "-1 to end"

--- src/Q09.groovy
+++ src/Q09.groovy
@@ -1,12 +1,15 @@
 /**
+ * Created by crk on 23/07/2016.
+ */
+/**
  * Created by crk on 22/07/2016.
  */
 Scanner input = new Scanner(System.in)
-println "Program to find highest number from sequence of positive numbers"
+println "Program to identify an increasing consecutive sequence of positive numbers"
 int number = 0
 int highestNumber = -1
 while (number != -1) {
-    print "Enter number (-1 to end): "
+    print "Enter number: "
     number = input.nextInt()
     if (number > highestNumber) {
         highestNumber = number
