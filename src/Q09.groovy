b77729c2abfb61e90427f640486e8f956f9704fe
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-07-24 00:08:38 +0100

    Q09 - code tidied up and checked

--- src/Q09.groovy
+++ src/Q09.groovy
@@ -1,18 +1,22 @@
 /**
  * Created by crk on 23/07/2016.
  */
-/**
- * Created by crk on 22/07/2016.
- */
+
 Scanner input = new Scanner(System.in)
 println "Program to identify an increasing consecutive sequence of positive numbers"
-int number = 0
-int highestNumber = -1
-while (number != -1) {
-    print "Enter number: "
-    number = input.nextInt()
-    if (number > highestNumber) {
-        highestNumber = number
+print "Enter number (-1 to end): "
+int previous = input.nextInt()
+// Initialise current to 0 to allow a later test for starting with a negative number
+int current = 0
+String increasing = "Yes"
+// Ends for any -ve number
+while (previous >= 0) {
+    print "Enter number (-1 to end): "
+    current = input.nextInt()
+    if (current - previous != 1 && current > -1) {
+        increasing = "No"
     }
+    previous = current
 }
-println "Highest number: " + highestNumber
+// If current == 0 then 1st number entered must have been negative
+println ((current != 0) ? increasing : "You started with a negative number")
